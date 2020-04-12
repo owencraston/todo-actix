8b509626a66898b1d82b22486b4ff06dcfff4911
Owen Craston <owen.craston@shopify.com>
2020-04-11 21:44:35 -0400

    connect to database, parse todos and return in response

--- src/config.rs
+++ src/config.rs
@@ -4,13 +4,16 @@ use config::ConfigError;
 #[derive(Deserialize)]
 pub struct ServerConfig {
     pub host: String,
-    pub port: i32
+    pub port: i32,
 }
+
 
 #[derive(Deserialize)]
 pub struct Config {
     pub server: ServerConfig,
+    pub pg: deadpool_postgres::Config,
 }
+
 
 impl Config {
     pub fn from_env() -> Result<Self, ConfigError> {
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,29 @@
 mod models;
 mod config;
+mod handlers;
+mod db;
 
-use crate::models::Status;
 use crate::config::Config;
-use actix_web::{HttpServer, App, web, Responder};
+use actix_web::{HttpServer, App, web};
 use std::io;
 use dotenv::dotenv;
-
-async fn status() -> impl Responder {
-    web::HttpResponse::Ok()
-        .json(Status {status: "Up".to_string()})
-}
+use tokio_postgres::NoTls;
+use crate::handlers::*;
 
 #[actix_rt::main]
 async fn main() -> io::Result<()> {
     dotenv().ok();
     let config = Config::from_env().unwrap();
 
+    let pool = config.pg.create_pool(NoTls).unwrap();
+
     println!("Starting server at http://{}:{}", config.server.host, config.server.port);
 
-    HttpServer::new(|| {
+    HttpServer::new(move || {
         App::new()
+            .data(pool.clone())
             .route("/", web::get().to(status))
+            .route("/todos{_:/?}", web::get().to(get_todos))
     })
     .bind(format!("{}:{}", config.server.host, config.server.port))?
     .run()
